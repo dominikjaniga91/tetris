4683f14844098d3b89094886c9e2ee8a81c9b53a
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-08 19:36:11 +0100

    Create a Painter class with paint method
    Method paint string using CSI and color identifier
    from ANSI Escape codes

---
+++ src/main/java/com/epam/prejap/tetris/game/Painter.java
@@ -0,0 +1,28 @@
+package com.epam.prejap.tetris.game;
+
+/**
+ * Class used to colour string character
+ *
+ * @author Dominik Janiga
+ * @version 0.2
+ * @since 0.2
+ */
+final class Painter {
+
+    private static final String ESCAPE = "\u001B[";
+    private static final String FINAL_BYTE = "m";
+    private static final String RESET_COLOR = ESCAPE + "0" + FINAL_BYTE;
+
+    /**
+     * Set up background color to empty string using Control Sequence Introducer
+     * from ANSI escape codes
+     *
+     * @param colorIdentifier color code from ANSI
+     * @return painted empty string
+     * @since 0.2
+     * @see <a href="https://en.wikipedia.org/wiki/ANSI_escape_code">ANSI Escape codes</a>
+     */
+    static String paint(int colorIdentifier) {
+        return ESCAPE + colorIdentifier + FINAL_BYTE + " " + RESET_COLOR;
+    }
+}
