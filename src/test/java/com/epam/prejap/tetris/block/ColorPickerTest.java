38ac65322245b43ede625d6b194323cda4d5a86d
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-08 20:15:59 +0100

    Test pick of color for specific block type

---
+++ src/test/java/com/epam/prejap/tetris/block/ColorPickerTest.java
@@ -0,0 +1,31 @@
+package com.epam.prejap.tetris.block;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+@Test(groups = "Color")
+public class ColorPickerTest {
+
+    @Test(dataProvider = "blocksWithColors")
+    public void colorPickerShouldReturnAppropriateColor(Block block, Color color) {
+        //given
+        int expected = color.getIdentifier();
+
+        //when
+        int actual = ColorPicker.pick(block);
+
+        //then
+        assertEquals(actual, expected, "Inappropriate color identifier ");
+    }
+
+    @DataProvider
+    private Object[][] blocksWithColors() {
+        return new Object[][]{
+                {new OBlock(), Color.RED},
+                {new LBlock(), Color.GREEN},
+                {new IBlock(), Color.BLUE}
+        };
+    }
+}
