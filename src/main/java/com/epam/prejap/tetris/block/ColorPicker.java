e6be6764bf8f788f132365d236aae17040fe34e9
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-09 10:30:32 +0100

    Improve java doc comments

--- src/main/java/com/epam/prejap/tetris/block/ColorPicker.java
+++ src/main/java/com/epam/prejap/tetris/block/ColorPicker.java
@@ -2,19 +2,19 @@ package com.epam.prejap.tetris.block;
 
 /**
  *
- * @author Dominik Janiga
- * @version 0.2
- * @since 0.2
+ * @author      Dominik Janiga
+ * @since       0.4
  */
 public final class ColorPicker {
 
     /**
      * Pick specific color identifier for each block type
      *
-     * @param block instance of specific game's block
-     * @return Color identifier from ANSI escape codes
-     * @since 0.2
-     * @see <a href="https://en.wikipedia.org/wiki/ANSI_escape_code">ANSI Escape codes</a>
+     * @param block     instance of specific game's block
+     * @return          color identifier from ANSI escape codes
+     * @since           0.4
+     * @see             Color
+     * @see             <a href="https://en.wikipedia.org/wiki/ANSI_escape_code">ANSI Escape codes</a>
      */
     public static byte pick(Block block) {
 
