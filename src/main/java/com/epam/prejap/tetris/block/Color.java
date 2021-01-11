1f09ed37537d9a49c11b80bda6a48c69d65a565b
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-01-11 19:42:39 +0100

    Remove methods from Color enum

--- src/main/java/com/epam/prejap/tetris/block/Color.java
+++ src/main/java/com/epam/prejap/tetris/block/Color.java
@@ -1,47 +1,32 @@
 package com.epam.prejap.tetris.block;
 
-import java.util.Arrays;
-
+/**
+ * @author Dominik Janiga
+ * @since 0.5
+ */
 public enum Color {
 
     BLACK(30),
     RED(31),
     GREEN(32),
     YELLOW(33),
-    BLUE(33),
+    BLUE(34),
     MAGENTA(35),
     CYAN(36),
     WHITE(37);
 
     /**
-     * Color identifier from ANSI escape codes
+     * Color identifiers from ANSI escape codes
+     *
      * @see <a href="https://en.wikipedia.org/wiki/ANSI_escape_code">ANSI Escape codes</a>
      */
     private final int colorId;
-    private final int backgroundColorId;
 
     Color(int colorId) {
         this.colorId =  colorId;
-        this.backgroundColorId = this.colorId +  10;
     }
 
-    byte getIdentifier() {
+    public byte getIdentifier() {
         return (byte) colorId;
-    }
-
-    public static Color of(int colorId) {
-        return Arrays.stream(values())
-                .filter(color -> color.colorId == colorId)
-                .findFirst()
-                .orElse(Color.BLACK);
-    }
-
-    public String applyFor(String blockMark) {
-
-        String escape =  "\u001B[";
-        String finalByte = "m";
-        String breakSign = ";";
-        String resetColor = escape + "0" + finalByte;
-        return escape + colorId + breakSign + backgroundColorId + finalByte + blockMark + resetColor;
     }
 }
